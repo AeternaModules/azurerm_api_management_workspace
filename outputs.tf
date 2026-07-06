1fcd7ca03d3da407f1aef5e006cd4c2ddb003c56
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "api_management_workspaces" {
   description = "All api_management_workspace resources"
   value       = azurerm_api_management_workspace.api_management_workspaces
 }
+output "api_management_workspaces_api_management_id" {
+  description = "List of api_management_id values across all api_management_workspaces"
+  value       = [for k, v in azurerm_api_management_workspace.api_management_workspaces : v.api_management_id]
+}
+output "api_management_workspaces_description" {
+  description = "List of description values across all api_management_workspaces"
+  value       = [for k, v in azurerm_api_management_workspace.api_management_workspaces : v.description]
+}
+output "api_management_workspaces_display_name" {
+  description = "List of display_name values across all api_management_workspaces"
+  value       = [for k, v in azurerm_api_management_workspace.api_management_workspaces : v.display_name]
+}
+output "api_management_workspaces_name" {
+  description = "List of name values across all api_management_workspaces"
+  value       = [for k, v in azurerm_api_management_workspace.api_management_workspaces : v.name]
+}
 
