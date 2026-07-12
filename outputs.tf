223c80a56b23a0e8e4d76f9f47d6ba6c3a2d6049
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "api_management_workspaces" {
-  description = "All api_management_workspace resources"
-  value       = azurerm_api_management_workspace.api_management_workspaces
-}
 output "api_management_workspaces_api_management_id" {
-  description = "List of api_management_id values across all api_management_workspaces"
-  value       = [for k, v in azurerm_api_management_workspace.api_management_workspaces : v.api_management_id]
+  description = "Map of api_management_id values across all api_management_workspaces, keyed the same as var.api_management_workspaces"
+  value       = { for k, v in azurerm_api_management_workspace.api_management_workspaces : k => v.api_management_id }
 }
 output "api_management_workspaces_description" {
-  description = "List of description values across all api_management_workspaces"
-  value       = [for k, v in azurerm_api_management_workspace.api_management_workspaces : v.description]
+  description = "Map of description values across all api_management_workspaces, keyed the same as var.api_management_workspaces"
+  value       = { for k, v in azurerm_api_management_workspace.api_management_workspaces : k => v.description }
 }
 output "api_management_workspaces_display_name" {
-  description = "List of display_name values across all api_management_workspaces"
-  value       = [for k, v in azurerm_api_management_workspace.api_management_workspaces : v.display_name]
+  description = "Map of display_name values across all api_management_workspaces, keyed the same as var.api_management_workspaces"
+  value       = { for k, v in azurerm_api_management_workspace.api_management_workspaces : k => v.display_name }
 }
 output "api_management_workspaces_name" {
-  description = "List of name values across all api_management_workspaces"
-  value       = [for k, v in azurerm_api_management_workspace.api_management_workspaces : v.name]
+  description = "Map of name values across all api_management_workspaces, keyed the same as var.api_management_workspaces"
+  value       = { for k, v in azurerm_api_management_workspace.api_management_workspaces : k => v.name }
 }
 
